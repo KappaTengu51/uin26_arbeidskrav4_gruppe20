3b3ca20f97f4e00dad84a243d7d607e70ddaed83
Ole Martin Hansen Kopperud <olemha@hiof.no>
2026-04-23 11:26:12 +0200

    Update OleProfile.jsx
    Small update

--- frontend/src/components/OleProfile.jsx
+++ frontend/src/components/OleProfile.jsx
@@ -8,6 +8,7 @@ export default function OleProfile() {
 				<img src={profileImage} alt="Illusrasjon for Ole" />
 			</figure>
 			<h2>Ole Martin Hansen Kopperud</h2>
+
 			<address>
 				<a href="mailto:olemha@hiof.no">olemha@hiof.no</a>
 			</address>
